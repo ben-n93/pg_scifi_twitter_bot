db40949177504fded67669e57206a85d0badb9fc
Ben <84557025+ben-n93@users.noreply.github.com>
2023-01-26 18:00:49 +1100

    Update twitter_bot.py
    Fixed while loop logic so the randomly chosen book check correctly works.

--- twitter_bot.py
+++ twitter_bot.py
@@ -18,7 +18,7 @@ BEARER_TOKEN = os.environ["PG_TWITTER_BEARER_TOKEN"]
 
 client = tweepy.Client(BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-# Pick random book to be posted to Twitter and check it hasn't already been posted previously.
+# Pick random book to be posted to Twitter.
 with open(SF_CATALOG) as sf_csv, open(IDS_CSV, "r") as IDs_csv:
     sf_csv_reader = csv.DictReader(sf_csv)
     sf_rows = [row for row in sf_csv_reader]
@@ -38,20 +38,22 @@ if sf_rows_count == IDs_rows_count:
 
 random_pick = sf_rows[random.randint(0, sf_rows_count)]
 
+# Check to make sure the book chosen hasn't already been posted previously.
 flag = False
 while flag == False:
     with open(IDS_CSV, "r") as f:
         csv_reader = csv.DictReader(f)
-        for row in csv_reader:
-            if random_pick["Text#"] == row["Text#"] or random_pick["Title"] == row["Title"]:
-                random_pick = rows[random.randint(0, rows_count)]
-                continue
-            else:
-                flag = True
-                book_pick = Book(random_pick["Text#"], random_pick["Title"],random_pick["Authors"])
-    with open(IDS_CSV, 'a') as f:
-        csv_writer = csv.DictWriter(f, HEADERS)
-        csv_writer.writerow({"Text#":book_pick.ID, "Title":book_pick.title})
+        text_rows = [row['Text#'] for row in csv_reader]
+        title_rows = [row['Title'] for row in csv_reader]
+    if random_pick["Text#"] in text_rows or random_pick["Title"] in title_rows:
+        random_pick = rows[random.randint(0, rows_count)]
+        continue
+    else:
+        flag = True
+        book_pick = Book(random_pick["Text#"], random_pick["Title"],random_pick["Authors"])
+        with open(IDS_CSV, 'a') as f:
+            csv_writer = csv.DictWriter(f, HEADERS)
+            csv_writer.writerow({"Text#":book_pick.ID, "Title":book_pick.title})
 
 # Post to Twitter.
 authors_string = " and ".join(book_pick.authors)
