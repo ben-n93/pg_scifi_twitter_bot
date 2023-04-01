13069e4e2a1ce6066f2bebec903c44b82e284305
Ben <84557025+ben-n93@users.noreply.github.com>
2023-04-01 14:12:40 +1100

    Update extract_gutenberg_feed.py
    removed superfluous code

--- src/extract_gutenberg_feed.py
+++ src/extract_gutenberg_feed.py
@@ -13,21 +13,17 @@ PG_CATALOG = "data/pg_catalog.csv"
 # Access Project Guntenberg CSV feed and write catalog to CSV file.
 URL = "https://www.gutenberg.org/cache/epub/feeds/pg_catalog.csv"
 
-with open(os.path.split(URL)[1], 'wb') as f, \
-        requests.get(URL, stream=True) as r:
-    for line in r.iter_lines():
+with open(PG_CATALOG, 'wb') as f:
+    content = requests.get(URL, stream=True)
+    for line in content.iter_lines():
         f.write(line+'\n'.encode())
 
-# Create/replace CSV file with science fiction books.
-os.remove(SF_CATALOG)
-
-with open(PG_CATALOG) as input_file, open(SF_CATALOG, 'a') as output_file:
-
+# Create CSV file of science fiction books.
+with open(PG_CATALOG) as input_file, open(SF_CATALOG, 'w') as output_file:
     field_names = ['Text#', 'Title', 'Authors']
     csv_reader = csv.DictReader(input_file)
     csv_writer = csv.DictWriter(output_file, field_names)
     csv_writer.writeheader()
-
     for row in csv_reader:
         if "Science Fiction" in row["Bookshelves"] and row["Type"] == "Text":
             csv_writer.writerow({"Text#":row["Text#"], "Title":row["Title"], "Authors":row["Authors"]})
