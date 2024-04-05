e006bed0313d4840ebf3088cb5f1cb1ddd6c6ee8
Ben <84557025+ben-n93@users.noreply.github.com>
2024-04-05 17:21:10 +1100

    Update twitter_bot.py
    Refactored code into function

--- src/twitter_bot.py
+++ src/twitter_bot.py
@@ -30,48 +30,53 @@ BEARER_TOKEN = os.environ["PG_TWITTER_BEARER_TOKEN"]
 
 client = tweepy.Client(BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, 
                        ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+def post_tweet():
+  """Pick a book and post to Twitter.
+  """
+  # Pick random book to be posted to Twitter.
+  with open(SF_CATALOG) as sf_csv, open(IDS_CSV) as IDs_csv:
+      sf_csv_reader = csv.DictReader(sf_csv)
+      sf_rows = [row for row in sf_csv_reader]
+      sf_rows_count = len(sf_rows)
+  
+      IDs_csv_reader = csv.DictReader(IDs_csv)
+      IDs_rows = [row for row in IDs_csv_reader]
+      IDs_rows_count = len(IDs_rows)
+  
+  # Check to see if the Twitterbot has gone through all books in the SF catalog.
+  if sf_rows_count == IDs_rows_count:
+      with open(IDS_CSV, "w") as f:
+          csv_writer = csv.DictWriter(f, HEADERS)
+          csv_writer.writeheader()
+          csv_writer.writerow({"Text#":"0", "Title":"0"})
+  
+  random_pick = sf_rows[random.randint(0, sf_rows_count)]
+  
+  # Check to make sure the book chosen hasn't already been posted previously.
+  books_to_log = []
+  flag = False
+  while flag is False:
+      with open(IDS_CSV) as f:
+          csv_reader = csv.DictReader(f)
+          text_rows = [row['Text#'] for row in csv_reader]
+          title_rows = [row['Title'] for row in csv_reader]
+      if random_pick["Title"] in title_rows and random_pick["Text#"] not in text_rows:
+          same_book = Book(random_pick["Text#"], random_pick["Title"],random_pick["Authors"])
+          books_to_log.append(same_book)
+      if random_pick["Text#"] in text_rows:
+          random_pick = sf_rows[random.randint(0, sf_rows_count)]
+          continue
+      flag = True
+      book_pick = Book(random_pick["Text#"], random_pick["Title"],random_pick["Authors"])
+      books_to_log.append(book_pick)
+      with open(IDS_CSV, 'a') as f:
+          csv_writer = csv.DictWriter(f, HEADERS)
+          for book in books_to_log:
+              csv_writer.writerow({"Text#":book.text_ID, "Title":book.title})
+  
+  # Post to Twitter.
+  authors_string = " and ".join(book_pick.authors)
+  client.create_tweet(text=f"Check out {book_pick.title} by {authors_string}. #ebook #sciencefiction {book_pick.URL}")
 
-# Pick random book to be posted to Twitter.
-with open(SF_CATALOG) as sf_csv, open(IDS_CSV) as IDs_csv:
-    sf_csv_reader = csv.DictReader(sf_csv)
-    sf_rows = [row for row in sf_csv_reader]
-    sf_rows_count = len(sf_rows)
-
-    IDs_csv_reader = csv.DictReader(IDs_csv)
-    IDs_rows = [row for row in IDs_csv_reader]
-    IDs_rows_count = len(IDs_rows)
-
-# Check to see if the Twitterbot has gone through all books in the SF catalog.
-if sf_rows_count == IDs_rows_count:
-    with open(IDS_CSV, "w") as f:
-        csv_writer = csv.DictWriter(f, HEADERS)
-        csv_writer.writeheader()
-        csv_writer.writerow({"Text#":"0", "Title":"0"})
-
-random_pick = sf_rows[random.randint(0, sf_rows_count)]
-
-# Check to make sure the book chosen hasn't already been posted previously.
-books_to_log = []
-flag = False
-while flag is False:
-    with open(IDS_CSV) as f:
-        csv_reader = csv.DictReader(f)
-        text_rows = [row['Text#'] for row in csv_reader]
-        title_rows = [row['Title'] for row in csv_reader]
-    if random_pick["Title"] in title_rows and random_pick["Text#"] not in text_rows:
-        same_book = Book(random_pick["Text#"], random_pick["Title"],random_pick["Authors"])
-        books_to_log.append(same_book)
-    if random_pick["Text#"] in text_rows:
-        random_pick = sf_rows[random.randint(0, sf_rows_count)]
-        continue
-    flag = True
-    book_pick = Book(random_pick["Text#"], random_pick["Title"],random_pick["Authors"])
-    books_to_log.append(book_pick)
-    with open(IDS_CSV, 'a') as f:
-        csv_writer = csv.DictWriter(f, HEADERS)
-        for book in books_to_log:
-            csv_writer.writerow({"Text#":book.text_ID, "Title":book.title})
-
-# Post to Twitter.
-authors_string = " and ".join(book_pick.authors)
-client.create_tweet(text=f"Check out {book_pick.title} by {authors_string}. #ebook #sciencefiction {book_pick.URL}")
+if __name__ == "__main__":
+  post_tweet()
